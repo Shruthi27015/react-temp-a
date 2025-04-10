8707b391a2506dd48bfb036f40e85efee0c9e9c4
shruthi27015 <dhudigamshruthi@gmail.com>
2025-04-10 13:49:37 +0530

    new

---
+++ src/components/App22.jsx
@@ -0,0 +1,13 @@
+// App22.jsx
+import React from "react";
+
+function App22({ name = "Guest", color = "blue" }) {
+    return (
+      <div style={{ color }}>
+        <h2>Hello, {name}!</h2>
+        <h2>{Age}!</h2>
+      </div>
+    );
+  }
+  
+  export default App22;
